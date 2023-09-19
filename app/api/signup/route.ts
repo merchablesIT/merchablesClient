43715c878fdaaecb96f78021d1a42584dbfd9fbf
ekanem david <ekanemd@live.com>
2023-09-19 02:33:54 +0100

    Check dotenv

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -15,6 +15,8 @@ const client = new MongoClient(uri, {
     }
 })
 
+console.log("process.env", process.env.MONGODB_USERNAME)
+
 async function run(email: string) {
     try {
       // Connect the client to the server	(optional starting in v4.7)
